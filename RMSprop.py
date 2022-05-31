1dc38d458965ad4976d2a5291e725a74a512a853
Zhahar <106292770+Zhahar@users.noreply.github.com>
2022-05-31 14:43:09 +0800

    Create RMSprop.py

---
+++ RMSprop.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  
+from matplotlib.colors import LogNorm
+import random
+
+f  = lambda x, y: (1.5 - x + x*y)**2 + (2.25 - x + x*y**2)**2 + (2.625 - x + x*y**3)**2
+
+minima = np.array([3., .5])
+minima_ = minima.reshape(-1, 1)
+
+xmin, xmax, xstep = -4.5, 4.5, .2
+ymin, ymax, ystep = -4.5, 4.5, .2
+x_list = np.arange(xmin, xmax + xstep, xstep)
+y_list = np.arange(ymin, ymax + ystep, ystep)
+x, y = np.meshgrid(x_list, y_list)
+z = f(x, y)
+
+df = lambda x: np.array( [2*(1.5 - x[0] + x[0]*x[1])*(x[1]-1) + 2*(2.25 - x[0] + x[0]*x[1]**2)*(x[1]**2-1)
+                                        + 2*(2.625 - x[0] + x[0]*x[1]**3)*(x[1]**3-1),
+                           2*(1.5 - x[0] + x[0]*x[1])*x[0] + 2*(2.25 - x[0] + x[0]*x[1]**2)*(2*x[0]*x[1]) 
+                                         + 2*(2.625 - x[0] + x[0]*x[1]**3)*(3*x[0]*x[1]**2)])
+
+def gradient_descent_RMSprop(df,x,alpha=0.01,beta = 0.9, iterations = 100,epsilon = 1e-8):
+    history=[x]   
+    v= np.ones_like(x)
+    for i in range(iterations):
+        if np.max(np.abs(df(x)))<epsilon:
+            print("梯度足够小！")
+            break
+        grad = df(x)       
+        v = beta*v+(1-beta)*grad**2       
+        x = x-alpha*grad/(np.sqrt(v)+epsilon)
+      
+        history.append(x)
+    return history
+
+x0=np.array([3., 4.])
+print("初始点",x0,"的梯度",df(x0))
+
+path = gradient_descent_RMSprop(df,x0,0.000005,0.99999999999,300000,1e-8)
+print(path[-1])
+path = np.asarray(path)  
+
+def plot_path(path,x,y,z,minima_,xmin, xmax,ymin, ymax):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.contour(x, y, z, levels=np.logspace(0, 5, 35), norm=LogNorm(), cmap=plt.cm.jet)
+    #ax.scatter(path[0],path[1]);
+    ax.quiver(path[:-1,0], path[:-1,1], path[1:,0]-path[:-1,0], path[1:,1]-path[:-1,1], scale_units='xy', angles='xy', scale=1, color='k')
+    ax.plot(*minima_, 'r*', markersize=18)
+
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')    
+    ax.set_xlim((xmin, xmax))
+    ax.set_ylim((ymin, ymax))
+
+path = np.asarray(path) 
+plot_path(path,x,y,z,minima_,xmin, xmax,ymin, ymax)
+
+
+plt.show()
